191eca3bcfdc03307e56f32affbeb7f6d28114a5
Shinsuke Takagi <shinny@ht.sfc.keio.ac.jp>
2016-02-23 17:18:22 +0900

    remove needless search functions

--- soxsearch/api/searcher.py
+++ soxsearch/api/searcher.py
@@ -54,65 +54,6 @@ class Searcher:
         return json_responce
 
 
-    def searchByLocation(self, latitude, longitude, radius):
-        self.data_manager.EstablishDBConnection()
-
-        # get how many degrees to move from the center point
-        degrees_to_move = dist2degree(radius)
-        rangecorners = {
-            "lat1": latitude - degrees_to_move["lat"],
-            "lng1": longitude - degrees_to_move["lng"],
-            "lat2": latitude + degrees_to_move["lat"],
-            "lng2": longitude + degrees_to_move["lng"]
-        }
-
-        # HERE MySQL search
-        sql = "select nodeID, id, type, Y(latlng), X(latlng) \
-            from nodelist where MBRContains(GeomFromText \
-            ('LineString(" \
-            + str(rangecorners["lat1"]) + " " \
-            + str(rangecorners["lng1"]) + ", " \
-            + str(rangecorners["lat2"]) + " " \
-            + str(rangecorners["lng2"]) + ")'), latlng)"
-
-
-        result = self.data_manager.fetchRecords(sql)
-        json_responce = self.createNodeListJSON(result)
-
-        self.data_manager.CloseDBConnection()
-
-        return json_responce
-
-
-    def searchByName(self, name):
-        self.data_manager.EstablishDBConnection()
-
-        # HERE MySQL search
-        sql = "select nodeID, id, type, Y(latlng), X(latlng) \
-            from nodelist where nodeID regexp \
-            '^.*" + name + ".*'"
-        result = self.data_manager.fetchRecords(sql)
-        json_responce = self.createNodeListJSON(result)
-
-        self.data_manager.CloseDBConnection()
-
-        return json_responce
-
-
-    def searchByType(self, sensorType):
-        self.data_manager.EstablishDBConnection()
-
-        # HERE MySQL search
-        sql = "select nodeID, id, type, Y(latlng), X(latlng) \
-            from nodelist where type='" + sensorType + "'"
-        result = self.data_manager.fetchRecords(sql)
-        json_responce = self.createNodeListJSON(result)
-
-        self.data_manager.CloseDBConnection()
-
-        return json_responce
-
-
     def createNodeListJSON(self, dictdata):
         nodelist_json = {"nodelist": [], "total": 0, "time": 0}
         for row in dictdata:
